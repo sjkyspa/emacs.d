5b37fec682a1380a03c1e3104750e7fae72f4fc0
sjkyspa <sjkyspa@gmail.com>
2019-02-20 10:13:39 +0800

    add markdown preview support

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,9 +3,12 @@
 ;;; Code:
 
 ;;; local
+(setq-default make-backup-files nil)
 
 (after-load 'projectile
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map))
+
+(setq markdown-command "jq --slurp --raw-input '{\"text\": \"\\(.)\", \"mode\": \"gfm\"}' | curl -sS --data @- https://api.github.com/markdown")
 
 (provide 'init-local)
 ;;; init-local.el ends here

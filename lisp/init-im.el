e5f8e33af7cbae7aefe014e0244cb831b9b8e8af
sjkyspa <sjkyspa@gmail.com>
2019-04-28 09:47:16 +0800

    try to fix pyim config

--- lisp/init-im.el
+++ lisp/init-im.el
@@ -4,62 +4,49 @@
 
 ;;; im
 
-;; (maybe-require-package 'pyim)
-;; (maybe-require-package 'pyim-basedict)
-;; (maybe-require-package 'posframe)
+(require-package 'pyim)
+(require-package 'pyim-basedict)
+(require-package 'posframe)
 
-;; (add-hook 'emacs-startup-hook
-;;           #'(lambda () (pyim-restart)))
-
-;; (after-load 'pyim
-;;   (setq-default default-input-method "pyim")
-;;   (setq-default pyim-default-scheme 'quanpin)
-;;   (setq-default pyim-page-length 8)
-;;   (pyim-isearch-mode 1)
-;;   )
-
-
-;; (after-load 'pyim
-;;   (setq pyim-page-tooltip 'posframe))
-
-;; (after-load 'pyim-basedict
-;;(pyim-basedict-enable))
-
-
-(when (maybe-require-package 'pyim)
-  (require-package 'pyim-basedict)
-  (require 'pyim)
-  (require 'pyim-basedict)
-  (require 'posframe)
-
+(after-load 'pyim
   (pyim-basedict-enable)
+  (setq-default pyim-default-scheme 'quanpin)
+  (setq-default pyim-page-length 5)
+  (pyim-isearch-mode 1)
+  (setq pyim-page-tooltip 'posframe)
+  (setq-default pyim-english-input-switch-functions
+                '(pyim-probe-dynamic-english
+                  pyim-probe-isearch-mode
+                  pyim-probe-program-mode
+                  pyim-probe-org-structure-template))
+  (setq-default pyim-punctuation-half-width-functions
+                '(pyim-probe-punctuation-line-beginning
+                  pyim-probe-punctuation-after-punctuation))
+  (setq default-input-method "pyim")
+  (global-set-key (kbd "C-\\") 'toggle-input-method)
+  (global-set-key (kbd "M-j") 'pyim-convert-string-at-point) ;与 pyim-probe-dynamic-english 配合
+  (global-set-key (kbd "M-f") 'pyim-forward-word)
+  (global-set-key (kbd "M-b") 'pyim-backward-word))
 
-  (after-load 'pyim
-    (setq-default pyim-default-scheme 'quanpin)
-    (setq-default pyim-page-length 5)
-    (pyim-isearch-mode 1)
-    (setq pyim-page-tooltip 'posframe)
-    (setq-default pyim-english-input-switch-functions
-                  '(pyim-probe-dynamic-english
-                    pyim-probe-isearch-mode
-                    pyim-probe-program-mode
-                    pyim-probe-org-structure-template))
-    (setq-default pyim-punctuation-half-width-functions
-                  '(pyim-probe-punctuation-line-beginning
-                    pyim-probe-punctuation-after-punctuation))
-    (setq default-input-method "pyim")
-    (global-set-key (kbd "C-\\") 'toggle-input-method)
-    (global-set-key (kbd "M-j") 'pyim-convert-string-at-point) ;与 pyim-probe-dynamic-english 配合
-    (global-set-key (kbd "M-f") 'pyim-forward-word)
-    (global-set-key (kbd "M-b") 'pyim-backward-word)
-    )
-
+(add-hook 'emacs-startup-hook #'(lambda () (pyim-restart)))
 
 
-  (add-hook 'emacs-startup-hook
-            #'(lambda () (pyim-restart)))
 
+(setq rime_share_data_dir "/Library/Input Methods/Squirrel.app/Contents/SharedSupport")
+(setq rime_user_data_dir "/Users/morpheus/Library/Rime")
+(add-to-list 'load-path "/Users/morpheus/.oh-my-env/06-emacs.d/liberime/build")
+(load "/Users/morpheus/.oh-my-env/06-emacs.d/liberime/build/liberime.so")
+
+(require 'liberime)
+(defun setup-liberime()
+  ;; incase hooks not running
+  (interactive)
+  (liberime-start rime_share_data_dir rime_user_data_dir)
+  (liberime-select-schema "luna_pinyin_simp")
+  (setq pyim-default-scheme 'rime)
   )
+;; work with pyim
+(add-hook 'pyim-load-hook 'setup-liberime) ;; or set with use-package
 
 
 (provide 'init-im)
